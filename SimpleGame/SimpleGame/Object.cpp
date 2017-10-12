f3b9eebab11e6386f8da67cc14a86976a8b07e81
Hong Seong Jun <zzuny618@gmail.com>
2017-10-12 15:21:09 +0900

    second

--- SimpleGame/SimpleGame/Object.cpp
+++ SimpleGame/SimpleGame/Object.cpp
@@ -1,9 +1,15 @@
 #include "stdafx.h"
 #include "Object.h"
 
+<<<<<<< HEAD
+Object::Object(float x1, float y1, float z1, float size1, float r1, float g1, float b1, float a1, float velo1, float vecX1, float vecY1)
+{
+	x = x1;
+=======
 Object::Object(float dfg, float y1, float z1, float size1, float r1, float g1, float b1, float a1)
 {
 	x = dfg;
+>>>>>>> 2b28763255150fb4e7e2b929db0573b1a75e2374
 	y = y1;
 	z = z1;
 	size = size1;
@@ -11,8 +17,21 @@ Object::Object(float dfg, float y1, float z1, float size1, float r1, float g1, f
 	g = g1;
 	b = b1;
 	a = a1;
+<<<<<<< HEAD
+	velo = velo1;
+	vecX = vecX1; 
+	vecY = vecY1;
 }
 
+void Object:: update()
+{
+	x = x+vecX*velo;
+	//x = x + 1;
+}
+=======
+}
+
+>>>>>>> 2b28763255150fb4e7e2b929db0573b1a75e2374
 
 Object::~Object()
 {
--- SimpleGame/SimpleGame/Object.h
+++ SimpleGame/SimpleGame/Object.h
@@ -3,8 +3,14 @@ class Object
 {
 
 public:
+<<<<<<< HEAD
+	float x, y, z, size, r, g, b, a ,velo, vecX, vecY;
+	Object(float x, float y, float z, float size, float r, float g, float b, float a, float velo, float vecX, float vecY);
+	void update();
+=======
 	float x, y, z, size, r, g, b, a;
 	Object(float x, float y, float z, float size, float r, float g, float b, float a);
+>>>>>>> 2b28763255150fb4e7e2b929db0573b1a75e2374
 	~Object();
 };
 
